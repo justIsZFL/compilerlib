2a798832d47fb34a0f675ad74f9c465f052274ca
zfl <zhangfuliang@xiaoyouzi.com>
2019-04-25 14:26:43 +0800

    first commit

---
+++ app/src/main/java/cn/exp/zfl/comlib/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.exp.zfl.comlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.zfl.aptlib.Test;
+
+@Test(path = "main")
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
